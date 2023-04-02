ea033f9b9e0045004e5c2f1f61de1972baab4e16
Billie Amit <biliamit@gmail.com>
2023-04-02 12:59:35 +0300

    add tailwind with custom base colors

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(179, 39%, 40%)",
+        secondary: "hsl(180, 38%, 77%)",
+        bg: "hsl(180, 36%. 96%)"
+      }
+    }
+  },
+  plugins: []
+};
