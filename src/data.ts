545e5d9f76840c59bc08a1ebc4e86e70d00867f9
Billie Amit <biliamit@gmail.com>
2023-04-25 15:59:21 +0300

    add home to the large devices menu

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,9 @@
 export const navLinks: { name: string; path: string }[] = [
   {
+    name: "בית",
+    path: "/"
+  },
+  {
     name: "למה אנחנו?",
     path: "/למה-אנחנו/"
   },
@@ -10,49 +14,5 @@ export const navLinks: { name: string; path: string }[] = [
   {
     name: "אודות",
     path: "/אודות/"
-  }
-];
-
-export const features: {
-  color: string;
-  icon: string;
-  line1: string;
-  line2: string;
-}[] = [
-  {
-    color: "bg-red-100",
-    icon: "mdi:account-sync-outline",
-    line1: "טיפול שוטף",
-    line2: "במשאבי האנוש"
-  },
-  {
-    color: "bg-yellow-100",
-    icon: "mdi:account-group-outline",
-    line1: "גיוס",
-    line2: "עובדים ומנהלים"
-  },
-  {
-    color: "bg-green-100",
-    icon: "mdi:cash-multiple",
-    line1: "ניהול",
-    line2: "מערך השכר"
-  },
-  {
-    color: "bg-purple-100",
-    icon: "mdi:clock-time-eight-outline",
-    line1: "טיפול",
-    line2: "במערך הנוכחות"
-  },
-  {
-    color: "bg-teal-100",
-    icon: "mdi:chart-line",
-    line1: "תמיכה",
-    line2: "במהלכי שינוי"
-  },
-  {
-    color: "bg-lime-100",
-    icon: "mdi:account-supervisor-outline",
-    line1: "ליווי והכשרה",
-    line2: "של מנהלים"
   }
 ];
