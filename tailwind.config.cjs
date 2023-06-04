9cca1ec2ebac35191bf6567d82bc2a03a6b2b6d4
Billie Amit <biliamit@gmail.com>
2023-06-04 13:47:43 +0300

    add css and tailwind basic settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,14 +5,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "green-logo": "hsl(179, 39%, 40%)",
-        "btn-green": "hsl(180, 38%, 77%)",
-        "bg-green": "hsl(180, 36%, 96%)",
-        "dark-blue": "hsl(214, 70%, 29%)",
-        "light-blue": "hsl(207, 54%, 52%)",
-        aqua: "hsl(162, 100%, 56%)",
-        "text-color": "hsl(225, 62%, 10%)",
-        salmon: "hsl(0, 94%, 80%)"
+        background: "hsl(var(--background))",
+        text: {
+          DEFAULT: "hsl(var(--text))",
+          offset: "hsl(var(--text-offset))"
+        },
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          light: "hsl(var(--accent-light))"
+        },
+        tertiary: {
+          DEFAULT: "hsl(var(--tertiary))",
+          bg: "hsl(var(--tertiary-bg))"
+        },
+        input: "hsl(var(--input))"
       },
       fontFamily: {
         sans: ["Rubik", ...defaultTheme.fontFamily.sans],
