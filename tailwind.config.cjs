3787977ee23aad65d1a09885c10a8cad640411b7
Billie Amit <biliamit@gmail.com>
2023-06-08 14:11:14 +0300

    add new basic styles settings - colors and fluid font sizes and spacing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,13 +18,30 @@ module.exports = {
         },
         tertiary: {
           DEFAULT: "hsl(var(--tertiary))",
-          bg: "hsl(var(--tertiary-bg))"
+          light: "hsl(var(--tertiary-light))"
         },
         input: "hsl(var(--input))"
       },
       fontFamily: {
         sans: ["Rubik", ...defaultTheme.fontFamily.sans],
         headings: ["Assistant", "sans-serif"]
+      },
+      fontSize: {
+        /* "fluid-0": "clamp(0.94rem, calc(0.92rem + 0.11vw), 1.00rem)",
+        "fluid-1": "clamp(1.13rem, calc(1.08rem + 0.22vw), 1.25rem)",
+        "fluid-2": "clamp(1.35rem, calc(1.28rem + 0.37vw), 1.56rem)",
+        "fluid-3": "clamp(1.62rem, calc(1.50rem + 0.58vw), 1.95rem)",
+        "fluid-4": "clamp(1.94rem, calc(1.77rem + 0.87vw), 2.44rem)",
+        "fluid-5": "clamp(2.33rem, calc(2.08rem + 1.25vw), 3.05rem)",
+        "fluid-6": "clamp(2.80rem, calc(2.45rem + 1.77vw), 3.82rem)",
+        "fluid-7": "clamp(3.36rem, calc(2.87rem + 2.45vw), 4.77rem)",
+        "fluid-8": "clamp(4.03rem, calc(3.36rem + 3.36vw), 5.96rem)" */
+        "fluid-1": "clamp(1.89rem, calc(1.48rem + 2.03vw), 2.93rem)",
+        "fluid-2": "clamp(3.27rem, calc(2.31rem + 4.79vw), 5.72rem)"
+      },
+      spacing: {
+        "fluid-1": "clamp(1.31rem, calc(0.65rem + 3.29vw), 3.00rem)",
+        "fluid-2": "clamp(1.31rem, calc(0.07rem + 6.22vw), 4.50rem)"
       },
       keyframes: {
         pageLoad: {
